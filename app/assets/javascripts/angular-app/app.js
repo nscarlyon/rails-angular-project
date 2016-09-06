b80ed5d6d2f59ecc77ccb15d3ba37b6a4dc79e20
nscarlyon <natasha.carlyon@gmail.com>
2016-09-06 18:46:27 -0400

    add home state to module

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -2,6 +2,10 @@ angular
   .module('app', ['ui.router'])
   .config(function($stateProvider) {
     $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/views/index.html'
+      })
       .state('artworks', {
         url: '/artworks',
         templateUrl: 'app/views/artworks/index.html'
