07322c1cd9d54cfdac400abb43bdd911a77aad5f
nscarlyon <natasha.carlyon@gmail.com>
2016-09-06 19:21:46 -0400

    add artworks new and create action

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,10 +1,23 @@
 class ArtworksController < ApplicationController
 
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+
+    if @artwork.valid?
+      @artwork.save
+      redirect_to artworks_path
+    end
+  end
+
   def show
     respond_to do |f|
       @artwork = Artwork.find(params[:id])
       f.json { render json: @artwork}
     end
   end
-  
+
 end
