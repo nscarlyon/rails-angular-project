b8c33065b5aa46f7b8808b0171aa4df08d847c01
nscarlyon <natasha.carlyon@gmail.com>
2016-09-06 14:39:17 -0400

    render artwork json in artworks show action

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,10 @@
 class ArtworksController < ApplicationController
+
+  def show
+    respond_to do |f|
+      @artwork = Artwork.find(params[:id])
+      f.json { render json: @artwork}
+    end
+  end
+  
 end
