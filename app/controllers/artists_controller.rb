87e5fa0c6e0782193cff015b9d24d150eab9622e
nscarlyon <natasha.carlyon@gmail.com>
2016-09-06 22:54:17 -0400

    add artist show json action

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,8 @@
 class ArtistsController < ApplicationController
+  def show
+    respond_to do |f|
+      @artist = Artist.find(params[:id])
+      f.json { render json: @artist}
+    end
+  end
 end
