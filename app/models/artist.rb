8244aa6696e383841e58c52736670a4c76d4ff0b
nscarlyon <natasha.carlyon@gmail.com>
2016-09-06 23:05:19 -0400

    remove associations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,4 @@
 class Artist < ApplicationRecord
   has_many :artworks
-  has_many :artwork_movements
   has_many :movements, through: :artworks
 end
--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -1,5 +1,4 @@
 class ArtworkSerializer < ActiveModel::Serializer
   attributes :id, :title, :medium, :date_seen, :artist_id
-  has_one :artist
-  has_many :movements
+
 end
