414c1f3fd5d5fa757947dd60cd85f313d7daa7ce
nscarlyon <natasha.carlyon@gmail.com>
2016-09-08 17:05:36 -0400

    assign this.movements in form controller

--- app/assets/javascripts/controllers/ArtworkFormController.js
+++ app/assets/javascripts/controllers/ArtworkFormController.js
@@ -1,7 +1,8 @@
-function ArtworkFormController($scope, $http, MovementsService) {
+function ArtworkFormController($scope, $http, movements) {
   $scope.submitArtwork = function() {
     $http.post('http://localhost:3000/artworks', $scope.artwork)
   }
+  this.movements = movements.data.movements
 }
 
 angular
