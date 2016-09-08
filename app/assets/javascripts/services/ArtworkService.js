4453f8811ba69e1a6e0bd5160f18daa839b73ee9
nscarlyon <natasha.carlyon@gmail.com>
2016-09-08 11:15:10 -0400

    add getArtworks function to Artwork service

--- app/assets/javascripts/services/ArtworkService.js
+++ app/assets/javascripts/services/ArtworkService.js
@@ -1,4 +1,9 @@
 function Artwork($http) {
+
+  this.getArtworks = function() {
+    return $http.get('http://localhost:3000/artworks')
+  }
+  
   this.getArtwork = function(id) {
     return $http.get('http://localhost:3000/artworks/' + id + '.json')
   }
