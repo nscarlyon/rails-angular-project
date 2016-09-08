e700f8764906438ca7ab36bec8ee4453154862fe
nscarlyon <natasha.carlyon@gmail.com>
2016-09-08 17:00:25 -0400

    add ng-repeat for movements in form; remove this.movements from ArtworkFormController

--- app/assets/javascripts/controllers/ArtworkFormController.js
+++ app/assets/javascripts/controllers/ArtworkFormController.js
@@ -2,8 +2,6 @@ function ArtworkFormController($scope, $http, MovementsService) {
   $scope.submitArtwork = function() {
     $http.post('http://localhost:3000/artworks', $scope.artwork)
   }
-
-  this.movements = MovementsService.getMovements()
 }
 
 angular
