a0fda2c02537c242c6268abd354251d1394c1690
nscarlyon <natasha.carlyon@gmail.com>
2016-09-08 20:31:40 -0400

    add toggleSelection function for movement checkboxes

--- app/assets/javascripts/controllers/ArtworkFormController.js
+++ app/assets/javascripts/controllers/ArtworkFormController.js
@@ -1,5 +1,16 @@
 function ArtworkFormController($scope, $http, movements) {
 
+  $scope.selection = [];
+
+  $scope.toggleSelection = function(movementId) {
+    var id = $scope.selection.indexOf(movementId)
+    if(id > -1){
+      $scope.selection.splice(id, 1)
+    } else {
+      $scope.selection.push(movementId)
+    }
+  }
+
   $scope.submitArtwork = function() {
     $scope.artwork.movements = $scope.selection
     debugger;
