3fc90f22d3f912be6d4d8ea84f8c0184e098995e
nscarlyon <natasha.carlyon@gmail.com>
2016-09-09 13:15:05 -0400

    pass artwork.data through ng-change for dateSeen; add .id to artowrk path url

--- app/assets/javascripts/controllers/ArtworkShowController.js
+++ app/assets/javascripts/controllers/ArtworkShowController.js
@@ -2,7 +2,7 @@ function ArtworkShowController(artwork, $scope, $http) {
   this.data = artwork.data["artwork"]
 
   $scope.updateDateSeen = function($scope) {
-    $http.patch('http://localhost:3000/artworks', $scope)
+    $http.patch('http://localhost:3000/artworks/' + $scope.id, $scope)
   }
 }
 
