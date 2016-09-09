4c4546f929c3d131b75dc1350b1915a13b1ebe69
nscarlyon <natasha.carlyon@gmail.com>
2016-09-09 13:03:41 -0400

    add .patch request for dateSeen; add ng-change to dateSeen input field

--- app/assets/javascripts/controllers/ArtworkShowController.js
+++ app/assets/javascripts/controllers/ArtworkShowController.js
@@ -1,5 +1,9 @@
-function ArtworkShowController(artwork) {
+function ArtworkShowController(artwork, $scope, $http) {
   this.data = artwork.data["artwork"]
+
+  $scope.updateDateSeen = function($scope) {
+    $http.patch('http://localhost:3000/artworks', $scope)
+  }
 }
 
 angular
