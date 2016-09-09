edd1e89d09344008ba9ab72a478a4abb447e1859
nscarlyon <natasha.carlyon@gmail.com>
2016-09-09 16:46:32 -0400

    add .destroyArtwork function; pass artwork.data via ng-click for destroy artwork button

--- app/assets/javascripts/controllers/ArtworkShowController.js
+++ app/assets/javascripts/controllers/ArtworkShowController.js
@@ -4,6 +4,10 @@ function ArtworkShowController(artwork, $scope, $http) {
   $scope.updateDateSeen = function($scope) {
     $http.patch('http://localhost:3000/artworks/' + $scope.id, $scope)
   }
+
+  $scope.destroyArtwork = function($scope) {
+    $http.delete('http://localhost:3000/artworks/' + $scope.id)
+  }
 }
 
 angular
