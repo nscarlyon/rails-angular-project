b12bb6a2e93152db0d1873b03b713511199df379
nscarlyon <natasha.carlyon@gmail.com>
2016-09-08 20:31:03 -0400

    add attributes to movement input field

--- app/assets/javascripts/controllers/ArtworkFormController.js
+++ app/assets/javascripts/controllers/ArtworkFormController.js
@@ -1,5 +1,8 @@
 function ArtworkFormController($scope, $http, movements) {
+
   $scope.submitArtwork = function() {
+    $scope.artwork.movements = $scope.selection
+    debugger;
     $http.post('http://localhost:3000/artworks', $scope.artwork)
   }
   this.movements = movements.data.movements
