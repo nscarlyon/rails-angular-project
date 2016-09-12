3131009a2e6148d81b5dbcdcedbf9f8b848bf0d5
nscarlyon <natasha.carlyon@gmail.com>
2016-09-11 22:28:09 -0400

    add destroyArtwork function to Artwork service

--- app/assets/javascripts/services/ArtworkService.js
+++ app/assets/javascripts/services/ArtworkService.js
@@ -3,9 +3,13 @@ function Artwork($http) {
   this.getArtworks = function() {
     return $http.get('http://localhost:3000/artworks')
   }
-  
+
   this.getArtwork = function(id) {
     return $http.get('http://localhost:3000/artworks/' + id + '.json')
+  }
+
+  this.destroyArtwork = function(id) {
+    return $http.delete('http://localhost:3000/artworks/' + id))
   }
 }
 
