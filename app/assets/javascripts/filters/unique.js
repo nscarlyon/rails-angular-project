c158732645d7d52a68d72590cd23a7daf2807289
nscarlyon <natasha.carlyon@gmail.com>
2016-09-13 16:41:06 -0400

    add unique.js(outside source)

--- app/assets/javascripts/filters/unique.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('app')
-  .filter('unique', unique)
-
-  function unique() {
-    return function(array, field) {
-      return _.uniq(array, function(a) { return a[field]; });
-    };
-  }
