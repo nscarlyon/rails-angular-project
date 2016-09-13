ca9c522b644c08c59e5d137317608dbc8e14a113
nscarlyon <natasha.carlyon@gmail.com>
2016-09-13 13:34:51 -0400

    fix syntax errors in Date.js; modify ng-message for dob

--- app/assets/javascripts/directives/Date.js
+++ app/assets/javascripts/directives/Date.js
@@ -1,12 +1,12 @@
 angular
   .module('app')
-  .directive('Date', date)
+  .directive('Date', Date)
 
   function Date() {
     return {
       restrict: 'A',
       require: 'ngModel',
-      link: function (scope. element, attrs, ngModel) {
+      link: function (scope, element, attrs, ngModel) {
         ngModel.$validators.date = function(value) {
           return /\d{2}\d{2}\d{2}/i.test(value);
         };
