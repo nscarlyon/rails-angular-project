863845b66ba6039df6707b1982e4e330d2b9aab8
nscarlyon <natasha.carlyon@gmail.com>
2016-09-13 13:52:52 -0400

    change Date to date.js; require directives

--- app/assets/javascripts/directives/Date.js
+++ app/assets/javascripts/directives/Date.js
@@ -1,15 +1,15 @@
-angular
-  .module('app')
-  .directive('Date', Date)
-
-  function Date() {
+  function date() {
     return {
       restrict: 'A',
       require: 'ngModel',
       link: function (scope, element, attrs, ngModel) {
         ngModel.$validators.date = function(value) {
           return /\d{2}\d{2}\d{2}/i.test(value);
-        };
+        }
       }
     }
   }
+
+  angular
+    .module('app')
+    .directive('date', date)
