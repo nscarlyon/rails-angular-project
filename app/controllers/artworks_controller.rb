4422548287a10d7f09f29109173833237ebecadf
nscarlyon <natasha.carlyon@gmail.com>
2016-09-13 12:23:14 -0400

    delete new action; remove '@' from variables

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -3,47 +3,43 @@ class ArtworksController < ApplicationController
   skip_before_action  :verify_authenticity_token
 
   def index
-    @artworks = Artwork.all
-    render json: @artworks
-  end
-
-  def new
-    @artwork = Artwork.new
+    artworks = Artwork.all
+    render json: artworks
   end
 
   def create
-    @artwork = Artwork.new(artwork_params)
-    @artwork.artist = Artist.find_or_create_by(name: params["artist"]["name"])
-    @artwork.movement_ids = params["movements"]
+    artwork = Artwork.new(artwork_params)
+    artwork.artist = Artist.find_or_create_by(name: params["artist"]["name"])
+    artwork.movement_ids = params["movements"]
 
     if params["movement"] && params["movement"] != ""
-      @artwork.movements << Movement.find_or_create_by(name: params["movement"].capitalize)
+      artwork.movements << Movement.find_or_create_by(name: params["movement"].capitalize)
     end
-    @artwork.save
+    artwork.save
   end
 
   def show
     respond_to do |f|
-      @artwork = Artwork.find(params[:id])
-      f.json { render json: @artwork}
+      artwork = Artwork.find(params[:id])
+      f.json { render json: artwork}
     end
   end
 
   def update
-    @artwork = Artwork.find(params[:id])
-    @artwork.update(artwork_params)
-    @artwork.artist = Artist.find_or_create_by(name: params["artist"]["name"])
-    @artwork.movement_ids = params["movements"]
+    artwork = Artwork.find(params[:id])
+    artwork.update(artwork_params)
+    artwork.artist = Artist.find_or_create_by(name: params["artist"]["name"])
+    artwork.movement_ids = params["movements"]
     if params["movement"] && params["movement"] != ""
-      @artwork.movements << Movement.find_or_create_by(name: params["movement"].capitalize)
+      artwork.movements << Movement.find_or_create_by(name: params["movement"].capitalize)
     end
 
-    @artwork.save
+    artwork.save
   end
 
   def destroy
-    @artwork = Artwork.find(params[:id])
-    @artwork.destroy
+    artwork = Artwork.find(params[:id])
+    artwork.destroy
   end
 
   private
