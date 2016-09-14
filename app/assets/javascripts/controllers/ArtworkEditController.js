8e512b1043a67ec1d14d4c68b83ed0eef8259ba0
nscarlyon <natasha.carlyon@gmail.com>
2016-09-13 21:42:12 -0400

    move relevant code from ArtworkNewController to ArtworkWeditController

--- app/assets/javascripts/controllers/ArtworkEditController.js
+++ app/assets/javascripts/controllers/ArtworkEditController.js
@@ -3,5 +3,39 @@ angular
   .controller('ArtworkEditController', ArtworkEditController)
 
   function ArtworkEditController(artwork, $scope, movements, Artwork) {
+    var vm = this;
+    vm.data = artwork.data["artwork"];
+    vm.selection = [];
+
+    vm.movementChecked =  movementChecked;
+    vm.toggleSelection = toggleSelection;
+    vm.submitArtwork = submitArtwork;
+
+    vm.data.movements.forEach(function(movement) {
+      vm.selection.push(movement.id);
+    });
+
+    function movementChecked(id) {
+      var value;
+
+      vm.data.movements.forEach(function(movement) {
+        if(id === movement.id) {value = movement.id}
+      })
+      return value
+    }
+
+    function toggleSelection(movementId) {
+      var id = vm.selection.indexOf(movementId)
+      if(id > -1){
+        vm.selection.splice(id, 1)
+      } else {
+        vm.selection.push(movementId)
+      }
+    }
+
+    function submitArtwork(artwork) {
+      artwork.movements = vm.selection
+      Artwork.updateArtwork(artwork)
+    }
 
   }
--- app/assets/javascripts/controllers/ArtworkNewController.js
+++ app/assets/javascripts/controllers/ArtworkNewController.js
@@ -4,31 +4,14 @@ angular
 
   function ArtworkFormController(artwork, $scope, movements, Artwork) {
     var vm = this;
-    vm.selection = [];
-    vm.movements = movements.data.movements;
-    vm.movementChecked =  movementChecked;
-    vm.toggleSelection = toggleSelection;
-    vm.submitArtwork = submitArtwork;
     vm.data = artwork
     vm.data.movements = []
+    vm.selection = [];
 
-      } else {
-        vm.data = artwork.data["artwork"];
+    vm.movements = movements.data.movements;
+    vm.toggleSelection = toggleSelection;
+    vm.submitArtwork = submitArtwork;
 
-        vm.data.movements.forEach(function(movement) {
-          vm.selection.push(movement.id);
-        });
-      }
-    }
-
-    function movementChecked(id) {
-      var value;
-
-      vm.data.movements.forEach(function(movement) {
-        if(id === movement.id) {value = movement.id}
-      })
-      return value
-    }
 
     function toggleSelection(movementId) {
       var id = vm.selection.indexOf(movementId)
@@ -41,11 +24,6 @@ angular
 
     function submitArtwork(artwork) {
       artwork.movements = vm.selection
-
-      if(artwork.id === undefined) {
-        Artwork.createArtwork(artwork)
-      } else {
-        Artwork.updateArtwork(artwork)
-      }
+      Artwork.createArtwork(artwork)
     }
 }
