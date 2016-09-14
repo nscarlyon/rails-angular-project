8e599bdd8621eaee6258eaf74efc975e959e507f
nscarlyon <natasha.carlyon@gmail.com>
2016-09-14 10:15:38 -0400

    add artistIndex state

--- app/assets/javascripts/artistApp.js
+++ app/assets/javascripts/artistApp.js
@@ -2,6 +2,16 @@ angular
   .module('artistApp', ['ui.router', 'templates', 'ngMessages'])
   .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
+    .state('artistIndex', {
+      url: '/artists',
+      templateUrl: 'artists/index.html',
+      controller: 'ArtistIndexController as artists',
+      resolve: {
+        artists: function(Artist) {
+          return Artist.getArtists()
+        }
+      }
+    })
     .state('artistShow', {
       url: '/artists/:id',
       templateUrl: 'artists/show.html',
