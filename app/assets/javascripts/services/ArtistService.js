48ba0645f615a95b106c69893baacfebf0ffbffa
nscarlyon <natasha.carlyon@gmail.com>
2016-09-13 22:20:03 -0400

    add getArtists function to Artist service

--- app/assets/javascripts/services/ArtistService.js
+++ app/assets/javascripts/services/ArtistService.js
@@ -1,5 +1,9 @@
 function Artist($http) {
 
+  this.getArtists = function() {
+    return $http.get('http://localhost:3000/artists')
+  }
+
   this.getArtist = function(id) {
     return $http.get('http://localhost:3000/artists/' + id + '.json')
   }
