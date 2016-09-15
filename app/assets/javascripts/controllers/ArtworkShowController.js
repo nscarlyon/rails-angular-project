d9929a8cd0145b082307b6e149b511c8c259b24b
nscarlyon <natasha.carlyon@gmail.com>
2016-09-15 11:10:24 -0400

    call Movements.createMovement service in Showcontroller

--- app/assets/javascripts/controllers/ArtworkShowController.js
+++ app/assets/javascripts/controllers/ArtworkShowController.js
@@ -2,15 +2,15 @@ angular
   .module('app')
   .controller('ArtworkShowController', ArtworkShowController)
 
-  function ArtworkShowController(artwork, $scope, Artwork) {
+  function ArtworkShowController(artwork, $scope, Artwork, Movements) {
     this.data = artwork.data["artwork"]
 
     this.updateDateSeen = function(artwork) {
       Artwork.updateArtwork(artwork)
     }
 
-    this.addMovement = function(artwork) { 
-      Artwork.updateArtwork(artwork)
+    this.addMovement = function(artwork) {
+      Movements.createMovement(artwork)
     }
 
     this.destroyArtwork = function($scope) {
