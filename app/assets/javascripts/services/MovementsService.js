2e3044a2cac9b73f0cf7a32da407aedda14f2e2d
nscarlyon <natasha.carlyon@gmail.com>
2016-09-15 11:08:50 -0400

    add createMovement function to Movements service

--- app/assets/javascripts/services/MovementsService.js
+++ app/assets/javascripts/services/MovementsService.js
@@ -2,6 +2,10 @@ function Movements($http) {
   this.getMovements = function() {
     return $http.get('http://localhost:3000/movements.json')
   }
+
+  this.createMovement = function() {
+    return $http.post('http://localhost:3000/movements')
+  }
 }
 
 angular
