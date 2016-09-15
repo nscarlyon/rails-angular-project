97982ab2f17a5831ac17c87175da773358f125e7
nscarlyon <natasha.carlyon@gmail.com>
2016-09-15 11:06:48 -0400

    add movement create action

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -5,4 +5,11 @@ class MovementsController < ApplicationController
       f.json { render json: @movements}
     end
   end
+
+  def create
+    artwork = Artwork.find(params["id"])
+    movement = Movement.find_or_create_by(name: params["movement"].titleize)
+    artwork.movements << movement
+    artwork.save
+  end
 end
