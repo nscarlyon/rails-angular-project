3636c154227c8d47afc02ac1c53ec083795f110f
nscarlyon <natasha.carlyon@gmail.com>
2016-09-22 18:20:56 -0400

    angular successfully displays filtered artworks

--- app/assets/javascripts/controllers/SearchRailsController.js
+++ app/assets/javascripts/controllers/SearchRailsController.js
@@ -5,8 +5,12 @@ angular
   function SearchRailsController(Artwork) {
     var vm = this
 
-    this.submit = function(location) {
-       Artwork.getArtworksLocation(location).success(function(res) {
+    this.submit = function(location, movement, title) {
+      l = location.toLowerCase()
+      m = movement.toLowerCase()
+      t = title.toLowerCase().replace(/ /g, '-')
+
+       Artwork.searchRails(l, m, t).success(function(res) {
          vm.artworks = res.artworks
        })
     }
