a728182a5380e69b3ea010d8a07d041b6599aab6
nscarlyon <natasha.carlyon@gmail.com>
2016-09-22 17:02:25 -0400

    location filter works

--- app/assets/javascripts/controllers/SearchRailsController.js
+++ app/assets/javascripts/controllers/SearchRailsController.js
@@ -2,11 +2,12 @@ angular
   .module('app')
   .controller('SearchRailsController', SearchRailsController)
 
-  function SearchRailsController(Artwork, $scope) {
+  function SearchRailsController(Artwork) {
+    var vm = this
 
-    this.submit = function() {
-       Artwork.getArtworksLocation($scope.location).success(function(res) {
-         $scope.artworks = res
+    this.submit = function(location) {
+       Artwork.getArtworksLocation(location).success(function(res) {
+         vm.artworks = res.artworks
        })
     }
   }
